523e996dedbdca6b3afd85cf961919d9fa8d3e67
abel2107 <mrabelalemu@gmail.com>
2022-06-08 11:01:21 -0700

    for all

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,6 +6,14 @@
  */
 int main(void)
 {
-	puts("_putchat");
+	
+	_putchar("p");
+	_pitchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+
 	return (0);
 }
